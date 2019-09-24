d70c37ca74c94cfdf3888cc8a5f6f7c7c3e13484
Mojtaba118 <40440450+Mojtaba118@users.noreply.github.com>
2019-09-24 22:24:04 +0330

    Update mainTest.js

--- test/mainTest.js
+++ test/mainTest.js
@@ -1,7 +1,22 @@
 const assert = require("assert");
-const PN = require("./../dist/persianNumber.min");
+const PN = require("./../src/persianNumber");
 
 describe("test main functions", () => {
+  it("number is converted to persian letters", () => {
+    assert.equal(PN.convert(123), "صد و بیست و سه");
+  });
+
+  it("decimal numbers is converted to persian letters", () => {
+    assert.equal(PN.convert(123.25), "صد و بیست و سه ممیز بیست و پنج صدم");
+  });
+
+  it("negative and percent and decimal numbers is converted to persian letters", () => {
+    assert.equal(
+      PN.convert("-123.25%"),
+      "منفی صد و بیست و سه ممیز بیست و پنج صدم درصد"
+    );
+  });
+
   it("number is converted to persian letters", () => {
     assert.equal(PN.convert(123), "صد و بیست و سه");
   });
